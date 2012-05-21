f31f9c3b87923ae2339a855623c6c86bbb08b193
Sergei Lebedev <superbobry@gmail.com>
2012-05-22 00:12:29 +0400

    Added 'cljsbuild' to 'project.clj' and bumped version

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
-(defproject waltz "0.1.0-alpha1"
+(defproject waltz "0.1.1"
   :description "A ClojureScript library to keep your state in step"
-  :dependencies [[clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :plugins [[lein-cljsbuild "0.1.9"]]
+  :cljsbuild {:builds [{:source-path "src"
+                        :compiler {:output-to "out/waltz.js"
+                                   :output-dir "out"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
