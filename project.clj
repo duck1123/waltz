3a6a7ea13cf51c584bfd57227b93c460905be64d
Dmitry Groshev <groshev@selectel.ru>
2012-05-22 16:16:45 +0400

    bump cljsbuild version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject waltz "0.1.1"
   :description "A ClojureScript library to keep your state in step"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.1.9"]]
+  :plugins [[lein-cljsbuild "0.1.10"]]
   :cljsbuild {:builds [{:source-path "src"
                         :compiler {:output-to "out/waltz.js"
                                    :output-dir "out"
