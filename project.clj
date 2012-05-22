d8f03e4623babe3b1b6f8d154253e4a4e02fddc5
Sergei Lebedev <superbobry@gmail.com>
2012-05-22 11:22:46 +0400

    Bumped clojure version in 'project.clj'

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject waltz "0.1.1"
   :description "A ClojureScript library to keep your state in step"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.1.9"]]
   :cljsbuild {:builds [{:source-path "src"
                         :compiler {:output-to "out/waltz.js"
