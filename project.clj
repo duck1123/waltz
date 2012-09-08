33d46f1962d29f5133f0adf4fcc7c061803f13f7
Daniel E. Renfer <duck@kronkltd.net>
2012-09-07 23:15:50 -0400

    remove lolg

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject net.kronkltd/waltz "0.1.2-SNAPSHOT"
   :description "A ClojureScript library to keep your state in step"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lolg "0.1.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.1.10"]]
   :cljsbuild {:builds [{:source-path "src"
                         :compiler {:output-to "out/waltz.js"
